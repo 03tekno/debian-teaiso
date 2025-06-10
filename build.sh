ad6c89375f197b214fde938e336485b18f460636
mobilturka <162546868+03tekno@users.noreply.github.com>
2025-06-10 09:12:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Muslim Linux iso build profile
+# MT Debian Testing Cinnamon Desktop Edition Linux iso build profile
 cd teaiso
 sudo apt update
 sudo apt install xorriso grub-pc-bin grub-efi mtools make python3 dosfstools e2fsprogs squashfs-tools python3-yaml gcc wget curl unzip xz-utils zstd debootstrap -y
 make && make install
-ln -s sid /usr/share/debootstrap/scripts/yirmibir || true
+ln -s sid /usr/share/debootstrap/scripts/testing || true
 cd ../
-mkteaiso --profile=./muslim --output=/output/ --debug 2>&1
+mkteaiso --profile=./debian --output=/output/ --debug 2>&1
